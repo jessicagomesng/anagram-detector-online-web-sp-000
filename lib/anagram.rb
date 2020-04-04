2562a6f75d55429c6dedb3e7511f9561c6776371
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-04 11:59:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
     array_of_matches = []
     #check if the lengths are the same
     array_of_anagrams.each do |possible_word|
-      if possible_word.length == @word.length
+    if possible_word.length == @word.length
           #turn possible_word into an array of letters
           possible_word_array = possible_word.split("")
           #see if array_of_letters includes each element of possible word
