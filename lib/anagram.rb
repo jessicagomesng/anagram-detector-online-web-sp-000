d1cc180fd4dae3f332dfd2b40a295eaf217021a7
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-04 12:00:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     array_of_letters = @word.split("")
     array_of_matches = []
     #check if the lengths are the same
-    array_of_anagrams.each do |possible_word|
+    array_of_anagrams.select do |possible_word|
       if possible_word.length == @word.length
           #turn possible_word into an array of letters
          possible_word_array = possible_word.split("")
